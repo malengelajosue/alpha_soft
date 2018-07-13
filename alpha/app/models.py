89b1cd07ad25c3b79a5e771f8496a361fb409480
josue <malengelajosue@gmail.com>
2018-07-14 01:16:35 +0200

    creations des tables(migrations)

--- alpha/app/models.py
+++ alpha/app/models.py
@@ -9,3 +9,22 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title +' '+self.body
+class Coordonates(models.Model):
+    lat=models.CharField(max_length=50)
+    long=models.CharField(max_length=50)
+    alt=models.CharField(max_length=50)
+    moment=models.CharField(max_length=50)
+    siteNumber=models.CharField(max_length=50)
+
+class Site(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.CharField(max_length=50)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+class myFiles(models.Model):
+    pathOf=models.CharField(max_length=50)
+    type=models.CharField(max_length=50)
+    siteNumber=models.CharField(max_length=50)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
